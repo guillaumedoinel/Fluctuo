c9c25e73b6f1a72cd2349837bccc3c8ae2dbd6e2
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-08-12 15:48:06 +0200

    Update Fluctuo.js

--- Fluctuo.js
+++ Fluctuo.js
@@ -9,8 +9,10 @@
 
     $.ajax({
       url: "https://api.multicycles.org/v1?access_token=25jBlbee9movEcBXSR7n2HQHkJLdHsmj",
-      type: "POST",
-      contentType: "application/json",
+      method: "POST",
+      headers: {
+        contentType: "application/json"
+      },
       data: //JSON.stringify(
         {"query":"query ($lat: Float!, $lng: Float!) {\n  providers(lat: $lat, lng: $lng) {\n    name\n    slug\n  }\n}\n","variables":{"lat":59.8939225,"lng":10.7150777}}
       //),
