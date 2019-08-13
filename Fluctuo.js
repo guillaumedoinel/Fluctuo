652fb3ef87fdb01a0ceeb649d48946067b3aa72f
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-08-13 12:05:38 +0200

    Update Fluctuo.js

--- Fluctuo.js
+++ Fluctuo.js
@@ -20,21 +20,27 @@
       ),
       dataType: "html",
       success: function(response) {
-        var providersJSON = response.data.providers;
+        /*var providersJSON = response.data.providers;
         var providersTableData = [];
         for (var i = 0, len = providersJSON.length; i < len; i++) {
           providersTableData.push({
             "name": providersJSON[i].name,
             "slug": providersJSON[i].slug
           });
-        }
+        }*/
+
         /*var providersTableData = [];
         providersTableData.push({
           "name": "successTOTO",
           "slug": "successTITI"
         });*/
-        p_table.appendRows(providersTableData);
-        //console.log("response.body");
+
+
+        //p_table.appendRows(providersTableData);
+        console.log("la function success est appellee");
+      },
+      error: function(response2) {
+        console.log("la function ERROR est appellee");
       }
     });
   }
