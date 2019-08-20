73759e572a2e4bcd7d8c713182d0bebc5260b934
DOINEL Guillaume <guillaume.doinel@alliance-rnm.com>
2019-08-20 16:38:10 +0200

    Update Fluctuo.js

--- Fluctuo.js
+++ Fluctuo.js
@@ -16,7 +16,8 @@
         "Content-Type": "application/json"
       },
       data: JSON.stringify(
-        {query:"query ($lat: Float!, $lng: Float!) {\n  providers(lat: $lat, lng: $lng) {\n    name\n    slug\n  }\n}\n",variables:{"lat":59.8939225,"lng":10.7150777}}
+        //{query:"query ($lat: Float!, $lng: Float!) {\n  providers(lat: $lat, lng: $lng) {\n    name\n    slug\n  }\n}\n",variables:{"lat":59.8939225,"lng":10.7150777}}
+        {query:"query ($lat: Float!, $lng: Float!) {  providers(lat: $lat, lng: $lng) {    name    slug  }}",variables:{"lat":59.8939225,"lng":10.7150777}}
       ),
       dataType: "json",
       success: function(response) {
